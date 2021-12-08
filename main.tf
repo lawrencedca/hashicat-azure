153081e0dd55ae47d62b37cf54593d29f746c489
Lawrence D <lawrenced_ca@outlook.com>
2021-12-08 02:13:44 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
 
   tags = {
     environment = "Production"
+    Billable  = "true"
   }
 }
 
@@ -111,10 +112,7 @@ resource "azurerm_virtual_machine" "catapp" {
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
 
-  tags = {
-    Billable  = "true"
-  }
-
+  
 
   storage_image_reference {
     publisher = var.image_publisher
